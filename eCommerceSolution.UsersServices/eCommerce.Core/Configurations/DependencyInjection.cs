eb7366cf123124ec304831ba4ba6da6ad2e97eb7
PauloAlves8039 <pj38alves@gmail.com>
2025-02-02 16:53:06 -0300

    feat: implements validation and register mapping

--- eCommerceSolution.UsersServices/eCommerce.Core/Configurations/DependencyInjection.cs
+++ eCommerceSolution.UsersServices/eCommerce.Core/Configurations/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using eCommerce.Core.ServiceContracts;
 using eCommerce.Core.Services;
+using eCommerce.Core.Validators;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.Core.Configurations;
@@ -9,6 +11,8 @@ public static class DependencyInjection
     public static IServiceCollection AddCore(this IServiceCollection services)
     {
         services.AddTransient<IUsersService, UsersService>();
+        services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+
         return services;
     }
 }
