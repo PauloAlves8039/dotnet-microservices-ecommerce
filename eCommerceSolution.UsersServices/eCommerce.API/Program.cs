d07a56fe0149ffe1f6699d821c45e16a9a668bc1
PauloAlves8039 <pj38alves@gmail.com>
2025-02-02 17:34:15 -0300

    feat: add swagger and cors settings with fluent validation

--- eCommerceSolution.UsersServices/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersServices/eCommerce.API/Program.cs
@@ -3,6 +3,7 @@ using eCommerce.Infrastructure.Configurations;
 using eCommerce.API.Middlewares;
 using System.Text.Json.Serialization;
 using eCommerce.Core.Mappers;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,21 @@ builder.Services.AddControllers().AddJsonOptions(options => {
 
 builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -22,6 +38,9 @@ var app = builder.Build();
 app.UseExceptionHandlingMiddleware();
 
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
