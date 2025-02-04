be9f3d00a63f9587490eca30bd78376d29deda97
PauloAlves8039 <pj38alves@gmail.com>
2025-02-04 12:05:15 -0300

    feat: create product entity and context class

--- eCommerceSolution.ProductsService/DataAccessLayer/Configurations/DependencyInjection.cs
+++ eCommerceSolution.ProductsService/DataAccessLayer/Configurations/DependencyInjection.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer.Configurations;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options => {
+            options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+        });
+
         return services;
     }
 }
