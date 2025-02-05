7894bb684f8cd557d32ef1839eedc81e9c8460cf
PauloAlves8039 <pj38alves@gmail.com>
2025-02-05 17:40:53 -0300

    feat: implements swagger and other configurations

--- eCommerceSolution.ProductsService/ProductsMicroService.API/Program.cs
+++ eCommerceSolution.ProductsService/ProductsMicroService.API/Program.cs
@@ -1,9 +1,11 @@
+using System.Text.Json.Serialization;
 using BusinessLogicLayer.Configurations;
 using DataAccessLayer.Configurations;
 using FluentValidation.AspNetCore;
+using ProductsMicroService.API.APIEndpoints;
+using ProductsMicroService.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddDataAccessLayer(builder.Configuration);
 builder.Services.AddBusinessLogicLayer();
@@ -12,11 +14,35 @@ builder.Services.AddControllers();
 
 builder.Services.AddFluentValidationAutoValidation();
 
-app.UseRouting();
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapProductAPIEndpoints();
 
 app.Run();
