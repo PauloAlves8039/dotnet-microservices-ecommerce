d4407ab45174cbcdfb7b1d040ddce88e00b5e6e8
PauloAlves8039 <pj38alves@gmail.com>
2025-02-14 14:42:55 -0300

    feat: create models classes and middleware with configuration

---
+++ eCommerceSolution.OrdersService/OrdersMicroservice.API/Middlewares/ExceptionHandlingMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersMicroservice.API.Middlewares
+{
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
--- eCommerceSolution.OrdersService/OrdersMicroservice.API/Program.cs
+++ eCommerceSolution.OrdersService/OrdersMicroservice.API/Program.cs
@@ -1,5 +1,7 @@
 using BusinessLogicLayer.Configurations;
 using DataAccessLayer.Configurations;
+using FluentValidation.AspNetCore;
+using OrdersMicroservice.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +10,30 @@ builder.Services.AddBusinessLogicLayer(builder.Configuration);
 
 builder.Services.AddControllers();
 
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-app.UseRouting();
+app.UseExceptionHandlingMiddleware();
 
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
